799b4f5d3ee9ec14554fedb2acbb510103a1845e
Marwan99 <marokhaled99@gmail.com>
2020-02-23 22:42:19 +0000

    Updated map resolution

--- scripts/map_pub.py
+++ scripts/map_pub.py
@@ -4,8 +4,8 @@ import numpy as np
 from visualization_msgs.msg import MarkerArray
 from nav_msgs.msg import OccupancyGrid
 
-map_pub = rospy.Publisher('2D_projected_map', OccupancyGrid, queue_size=10)
-map_res = 0.05
+map_pub = rospy.Publisher('projected_map_2D', OccupancyGrid, queue_size=10)
+map_res = 0.1
 
 map_message = OccupancyGrid()
 
@@ -17,7 +17,7 @@ def map_publisher_callbask(data):
     '''
     Why am I using data.markers[16] you may ask, I have no idea!
     for some reason that was the only marker getting filled with
-    significant amounts of usable data. Need to undersatnd why
+    significant amounts of usable data. Need to understand why
     this is the case.
     '''
     map_message.header.stamp = rospy.get_rostime()
@@ -45,15 +45,16 @@ def map_publisher_callbask(data):
 
     array = np.zeros(shape=(map_message.info.height, map_message.info.width))
     array.fill(0)
-
+    # rospy.loginfo(data.markers[16].points)
     for marker in data.markers[16].points:
+        
         x = int(round(((marker.x - min_x) / map_res)))
         x = min(x, map_message.info.width-1)
 
         y = int(round(((marker.y - min_y) / map_res)))
         y = min(y, map_message.info.height-1)
 
-        fixed_min_thresh = marker.z > (min_z+0.6) # decent results wiht 0.4
+        fixed_min_thresh = marker.z > (min_z+0.6) # decent results with 0.4
         avg_thresh = abs(marker.z - mean_z) > 0.6
 
         if avg_thresh:
@@ -62,14 +63,14 @@ def map_publisher_callbask(data):
             array[y][x] = 0
 
     map_message.data = array.astype(np.int8).reshape(-1)
-    rospy.loginfo("Published map " + str(max_x) +  " " + str(max_y) + " " + str(min_z))
     map_pub.publish(map_message)
+    
+    rospy.loginfo("Published map " + str(max_x) +  " " + str(max_y) + " " + str(min_z))
 
 def main():
     rospy.init_node('map_broadcaster', anonymous=True)
-
     rospy.Subscriber("occupied_cells_vis_array", MarkerArray, map_publisher_callbask)
-
+    
     rospy.spin()
 
 if __name__ == '__main__':
