a5e5069e6166d6e744fb0a3e65e56fee832cc101
Marwan Taher <marokhaled99@gmail.com>
2021-03-15 02:38:52 +0000

    linted

--- include/planner_3d.h
+++ include/planner_3d.h
@@ -1,69 +1,69 @@
-#include <ros/ros.h>
-#include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <visualization_msgs/Marker.h>
 #include <nav_core/base_global_planner.h>
+#include <nav_msgs/Path.h>
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
 
 #include <grid_map_msgs/GridMap.h>
 #include <grid_map_ros/grid_map_ros.hpp>
 
-#include <deque>
-#include <vector>
-#include <math.h>
 #include <algorithm>
 #include <boost/bind.hpp>
+#include <deque>
+#include <math.h>
+#include <vector>
 
 #include <octomap/octomap.h>
+#include <octomap_msgs/GetOctomap.h>
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
-#include <octomap_msgs/GetOctomap.h>
 
 #include <filters/filter_chain.h>
 
 #ifndef GLOBAL_PLANNER_CPP
 #define GLOBAL_PLANNER_CPP
 
-namespace planner_3d
-{
+namespace planner_3d {
 
-class Planner3D : public nav_core::BaseGlobalPlanner
-{
+class Planner3D : public nav_core::BaseGlobalPlanner {
 public:
-    Planner3D();
-    Planner3D(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+  Planner3D();
+  Planner3D(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
 
-    void initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
-    bool makePlan(const geometry_msgs::PoseStamped &start,
-                  const geometry_msgs::PoseStamped &goal,
-                  std::vector<geometry_msgs::PoseStamped> &plan);
-    
-    double factors[4]; // heuristic function coeffcients 
-    std::string elevation_layer;
-    std::string slope_layer;
+  void initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+  bool makePlan(const geometry_msgs::PoseStamped &start,
+                const geometry_msgs::PoseStamped &goal,
+                std::vector<geometry_msgs::PoseStamped> &plan);
+
+  double factors[4]; // heuristic function coeffcients
+  std::string elevation_layer;
+  std::string slope_layer;
 
 private:
-    grid_map::GridMap raw_gridmap;      // converted ocotomap to gridmap
-    grid_map::GridMap full_gridmap;     // Full size gridmap 40x80m (non-filtered)
-    grid_map::GridMap filtered_gridmap; // Filtered gridmap (based on Slope + inflation)
+  grid_map::GridMap raw_gridmap;  // converted ocotomap to gridmap
+  grid_map::GridMap full_gridmap; // Full size gridmap 40x80m (non-filtered)
+  grid_map::GridMap
+      filtered_gridmap; // Filtered gridmap (based on Slope + inflation)
 
-    grid_map_msgs::GridMap grid_map_message;
+  grid_map_msgs::GridMap grid_map_message;
 
-    grid_map::Index children[8] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+  grid_map::Index children[8] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+                                 {0, 1}, {0, -1}, {1, 0},  {-1, 0}};
 
-    filters::FilterChain<grid_map::GridMap> map_filter{"grid_map::GridMap"};
-    std::string filter_chain_params;
-    
-    ros::NodeHandle n;
-    ros::Publisher marker_publisher;
-    ros::Publisher full_map_publisher; // Publishes the full gridmap
-    ros::Publisher filtered_map_publisher;
-    ros::Subscriber global_map_subscrber;
+  filters::FilterChain<grid_map::GridMap> map_filter{"grid_map::GridMap"};
+  std::string filter_chain_params;
 
-    visualization_msgs::Marker marker;
+  ros::NodeHandle nh;
+  ros::Publisher marker_publisher;
+  ros::Publisher full_map_publisher; // Publishes the full gridmap
+  ros::Publisher filtered_map_publisher;
+  ros::Subscriber global_map_subscrber;
 
-    bool grid_map_initialized;
+  visualization_msgs::Marker marker;
 
-    void gridmap_callback(const grid_map_msgs::GridMapConstPtr &grid_map_msg_ptr);
+  bool grid_map_initialized;
+
+  void gridmap_callback(const grid_map_msgs::GridMapConstPtr &grid_map_msg_ptr);
 };
 }; // namespace planner_3d
 #endif
--- src/planner_3d.cpp
+++ src/planner_3d.cpp
@@ -1,284 +1,277 @@
-#include <pluginlib/class_list_macros.h>
 #include <planner_3d.h>
+#include <pluginlib/class_list_macros.h>
 
-//register this planner as a BaseGlobalPlanner plugin
+// register this planner as a BaseGlobalPlanner plugin
 PLUGINLIB_EXPORT_CLASS(planner_3d::Planner3D, nav_core::BaseGlobalPlanner)
 
-struct node
-{
-    grid_map::Position position;  // position in the odom frame (x, y) m
-    grid_map::Index index;  // index in the map 2D array
+struct node {
+  grid_map::Position position; // position in the odom frame (x, y) m
+  grid_map::Index index;       // index in the map 2D array
 
-    double g = 0;  // Number of cells from the start node
-    double h = 0;  // Distance from node to goal
-    double f = 0;  // cost function
-    double height = 0;  // height data in the cell
-    double slope=0;
+  double g = 0;      // Number of cells from the start node
+  double h = 0;      // Distance from node to goal
+  double f = 0;      // cost function
+  double height = 0; // height data in the cell
+  double slope = 0;
 
-    bool traversable = true;
+  bool traversable = true;
 
-    node *parent_node = NULL;
+  node *parent_node = NULL;
 
-    node() {}
+  node() {}
 
-    node(grid_map::Position pos, const grid_map::GridMap &map)
-    {
-        position = pos;
-        if (map.isInside(position))
-        {
-            map.getIndex(position, index);
-            height = map.at("elevation", index);
-            height = (std::isfinite(height)) ? height : 0;
-        }
+  node(grid_map::Position pos, const grid_map::GridMap &map) {
+    position = pos;
+    if (map.isInside(position)) {
+      map.getIndex(position, index);
+      height = map.at("elevation", index);
+      height = (std::isfinite(height)) ? height : 0;
     }
+  }
 
-    node(grid_map::Index ind, const grid_map::GridMap &map, std::string elevation_layer, std::string slope_layer)
-    {
-        index = ind;
-        if (map.isValid(index, elevation_layer))
-        {
-            map.getPosition(index, position);
+  node(grid_map::Index ind, const grid_map::GridMap &map,
+       std::string elevation_layer, std::string slope_layer) {
+    index = ind;
+    if (map.isValid(index, elevation_layer)) {
+      map.getPosition(index, position);
 
-            height = map.at("elevation", index);
-            height = (std::isfinite(height)) ? height : 0;
+      height = map.at("elevation", index);
+      height = (std::isfinite(height)) ? height : 0;
 
-            slope = map.at(slope_layer, index);
-            
-            // ROS_DEBUG("Slope: %0.3f", slope);
+      slope = map.at(slope_layer, index);
 
-            // Marking cell with a slope of bigger 45 deg as non-traversable
-            if(abs(slope) > 1.07)
-                traversable = false;
-        }
-        else
-            ROS_WARN_STREAM("Invalid index (" << ind[0] << ", " << ind[1] << ")");
-    }
+      // ROS_DEBUG("Slope: %0.3f", slope);
 
-    node(grid_map::Index ind)
-    {
-        index = ind;
-    }
+      // Marking cell with a slope of bigger 45 deg as non-traversable
+      if (abs(slope) > 1.07)
+        traversable = false;
+    } else
+      ROS_WARN_STREAM("Invalid index (" << ind[0] << ", " << ind[1] << ")");
+  }
 
-    void calc_f(node *parent, node goal, double k[])
-    {
-        parent_node = parent;
-        g = 1 + parent_node->g;
+  node(grid_map::Index ind) { index = ind; }
 
-        h = pow(goal.index(0) - index(0), 2) + pow(goal.index(1) - index(1), 2);
+  void calc_f(node *parent, node goal, double k[]) {
+    parent_node = parent;
+    g = 1 + parent_node->g;
 
-        f = k[0]*(g + h) + k[3]*slope;
-        // ROS_INFO("Cost function; g: %.2f, h: %.2f, f: %.2f", g , h, f);
-    }
+    h = pow(goal.index(0) - index(0), 2) + pow(goal.index(1) - index(1), 2);
+
+    f = k[0] * (g + h) + k[3] * slope;
+    // ROS_INFO("Cost function; g: %.2f, h: %.2f, f: %.2f", g , h, f);
+  }
 };
 
-bool get_path(node *cell, const geometry_msgs::PoseStamped &goal, std::vector<geometry_msgs::PoseStamped> &poses_list)
-{
-    geometry_msgs::PoseStamped temp_pos = goal;
+bool get_path(node *cell, const geometry_msgs::PoseStamped &goal,
+              std::vector<geometry_msgs::PoseStamped> &poses_list) {
+  geometry_msgs::PoseStamped temp_pos = goal;
 
-    while (cell != NULL && ros::ok())
-    {
-        temp_pos.pose.position.x = cell->position(0);
-        temp_pos.pose.position.y = cell->position(1);
-        temp_pos.pose.position.z = cell->height;
+  while (cell != NULL && ros::ok()) {
+    temp_pos.pose.position.x = cell->position(0);
+    temp_pos.pose.position.y = cell->position(1);
+    temp_pos.pose.position.z = cell->height;
 
-        poses_list.push_back(temp_pos);
-        cell = cell->parent_node;
-    }
-    std::reverse(poses_list.begin(), poses_list.end());
-    return true;
+    poses_list.push_back(temp_pos);
+    cell = cell->parent_node;
+  }
+  std::reverse(poses_list.begin(), poses_list.end());
+  return true;
 }
 
-bool node_sort(node i, node j)
-{
-    return i.f < j.f;
+bool node_sort(node i, node j) { return i.f < j.f; }
+
+int is_in_list(std::deque<node> list, node cell) {
+  for (int c = 0; c < list.size(); c++)
+    if (list[c].index(0) == cell.index(0) && list[c].index(1) == cell.index(1))
+      return c;
+  return -1;
 }
 
-int is_in_list(std::deque<node> list, node cell)
-{
-    for (int c = 0; c < list.size(); c++)
-        if (list[c].index(0) == cell.index(0) && list[c].index(1) == cell.index(1))
-            return c;
-    return -1;
+// Default Constructor
+namespace planner_3d {
+
+Planner3D::Planner3D() { ROS_WARN("Initialised!"); }
+
+Planner3D::Planner3D(std::string name, costmap_2d::Costmap2DROS *costmap_ros) {
+  initialize(name, costmap_ros);
 }
 
-//Default Constructor
-namespace planner_3d
-{
+void Planner3D::initialize(std::string name,
+                           costmap_2d::Costmap2DROS *costmap_ros) {
+  ROS_INFO("Initiallising...");
 
-Planner3D::Planner3D() { ROS_WARN("Initialised!");}
+  nh = ros::NodeHandle("~/" + name); // name: Planner3D
 
-Planner3D::Planner3D(std::string name, costmap_2d::Costmap2DROS *costmap_ros)
-{
-    initialize(name, costmap_ros);
+  marker_publisher =
+      nh.advertise<visualization_msgs::Marker>("/explored_cells", 1, true);
+  full_map_publisher =
+      nh.advertise<grid_map_msgs::GridMap>("/full_gridmap", 1, true);
+  filtered_map_publisher =
+      nh.advertise<grid_map_msgs::GridMap>("/filtered_gridmap", 1, true);
+
+  global_map_subscrber = nh.subscribe<grid_map_msgs::GridMap>(
+      "/grid_map_pcl_loader_node/grid_map_from_raw_pointcloud", 1,
+      &Planner3D::gridmap_callback, this);
+
+  nh.param<std::string>(name + "/elevation_layer", elevation_layer,
+                        "elevation");
+  nh.param<std::string>(name + "/slope_layer", slope_layer, "elevation");
+  nh.param<double>(name + "/factor_0", factors[0], 1);
+  nh.param<double>(name + "/factor_1", factors[1], 0);
+  nh.param<double>(name + "/factor_2", factors[2], 0);
+  nh.param<double>(name + "/factor_3", factors[3], 0);
+
+  grid_map_initialized = false;
+
+  // Configuring the filter chain
+  if (!map_filter.configure("/grid_map_filters", nh)) {
+    ROS_ERROR("Could not configure the filter chain.");
+    return;
+  } else
+    ROS_INFO("Map filter chain configured");
+
+  marker.header.frame_id = "map";
+  marker.ns = "cubes";
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::MODIFY;
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.1;
+  marker.scale.z = 0.1;
+  marker.color.a = 1.0; // Don't forget to set the alpha!
+  marker.color.r = 1.0;
+  marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+
+  ROS_INFO("planner_3D initiallised");
 }
 
-void Planner3D::initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros)
-{
-    ROS_INFO("Initiallising...");
+void Planner3D::gridmap_callback(
+    const grid_map_msgs::GridMapConstPtr &grid_map_msg_ptr) {
+  ROS_INFO("Global grid_map received");
 
-    nh = ros::NodeHandle("~/" + name); // name: Planner3D
+  grid_map::GridMapRosConverter::fromMessage(*grid_map_msg_ptr, raw_gridmap);
+  grid_map_initialized = true;
 
-    marker_publisher = nh.advertise<visualization_msgs::Marker>("/explored_cells", 1, true);
-    full_map_publisher = nh.advertise<grid_map_msgs::GridMap>("/full_gridmap", 1, true);
-    filtered_map_publisher = nh.advertise<grid_map_msgs::GridMap>("/filtered_gridmap", 1, true);
+  // grid_map::GridMapRosConverter::toMessage(raw_gridmap, grid_map_message);
+  // full_map_publisher.publish(grid_map_message);
 
-    global_map_subscrber = nh.subscribe<grid_map_msgs::GridMap>("/grid_map_pcl_loader_node/grid_map_from_raw_pointcloud", 1, &Planner3D::gridmap_callback, this);
+  // Filter grid map
+  // ROS_INFO("Filtering raw GridMap...");
+  // if (!map_filter.update(raw_gridmap, filtered_gridmap))
+  // {
+  //     ROS_ERROR("Could not update the grid map filter chain!");
+  //     return;
+  // }
 
-    nh.param<std::string>(name + "/elevation_layer", elevation_layer, "elevation");
-    nh.param<std::string>(name + "/slope_layer", slope_layer, "elevation");
-    nh.param<double>(name + "/factor_0", factors[0], 1);
-    nh.param<double>(name + "/factor_1", factors[1], 0);
-    nh.param<double>(name + "/factor_2", factors[2], 0);
-    nh.param<double>(name + "/factor_3", factors[3], 0);
+  // ROS_ERROR_STREAM(filtered_gridmap.getLayers()[0]);
+  // grid_map_message = *grid_map_msg_ptr;
+  // grid_map::GridMapRosConverter::toMessage(filtered_gridmap,
+  // grid_map_message); filtered_map_publisher.publish(grid_map_message);
 
-    grid_map_initialized = false;
-
-    // Configuring the filter chain
-    if (!map_filter.configure("/grid_map_filters", nh))
-    {
-        ROS_ERROR("Could not configure the filter chain.");
-        return;
-    }
-    else
-        ROS_INFO("Map filter chain configured");
-
-    marker.header.frame_id = "map";
-    marker.ns = "cubes";
-    marker.type = visualization_msgs::Marker::CUBE;
-    marker.action = visualization_msgs::Marker::MODIFY;
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.scale.z = 0.1;
-    marker.color.a = 1.0; // Don't forget to set the alpha!
-    marker.color.r = 1.0;
-    marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
-
-    ROS_INFO("planner_3D initiallised");
+  ROS_INFO("GridMap filtered");
 }
 
-void Planner3D::gridmap_callback(const grid_map_msgs::GridMapConstPtr &grid_map_msg_ptr)
-{
-    ROS_INFO("Global grid_map received");
-    
-    grid_map::GridMapRosConverter::fromMessage(*grid_map_msg_ptr, raw_gridmap);
-    grid_map_initialized = true;
+bool Planner3D::makePlan(const geometry_msgs::PoseStamped &start,
+                         const geometry_msgs::PoseStamped &goal,
+                         std::vector<geometry_msgs::PoseStamped> &plan) {
+  if (!grid_map_initialized)
+    return false;
 
-    // grid_map::GridMapRosConverter::toMessage(raw_gridmap, grid_map_message);
-    // full_map_publisher.publish(grid_map_message);
+  // Used to track the planner execution time
+  ros::Time time;
+  time = ros::Time::now();
 
-    // Filter grid map
-    // ROS_INFO("Filtering raw GridMap...");
-    // if (!map_filter.update(raw_gridmap, filtered_gridmap)) 
-    // {
-    //     ROS_ERROR("Could not update the grid map filter chain!");
-    //     return;
-    // }
+  ROS_INFO("Finding path...");
+  ROS_INFO("layer used: %s", slope_layer.c_str());
 
-    // ROS_ERROR_STREAM(filtered_gridmap.getLayers()[0]);
-    // grid_map_message = *grid_map_msg_ptr;
-    // grid_map::GridMapRosConverter::toMessage(filtered_gridmap, grid_map_message);
-    // filtered_map_publisher.publish(grid_map_message);
+  // Clear all the existing markers from the previous path
+  marker.type = visualization_msgs::Marker::DELETEALL;
+  marker_publisher.publish(marker);
 
-    ROS_INFO("GridMap filtered");
-}
+  // gridmap to use for finding the path
+  // grid_map::GridMap &search_gridmap = full_gridmap; //full_gridmap,
+  // raw_gridmap or filtered_gridmap
+  grid_map::GridMap search_gridmap = raw_gridmap;
 
-bool Planner3D::makePlan(const geometry_msgs::PoseStamped &start, const geometry_msgs::PoseStamped &goal, std::vector<geometry_msgs::PoseStamped> &plan)
-{
-    if(!grid_map_initialized)
-        return false;
+  node start_pos, goal_pos;
+  grid_map::Position pos;
 
-    // Used to track the planner execution time
-    ros::Time time;
-    time = ros::Time::now();
+  // Validating that the starting pose is within the map
+  pos = grid_map::Position(start.pose.position.x, start.pose.position.y);
+  if (search_gridmap.isInside(pos))
+    start_pos = node(pos, search_gridmap);
+  else {
+    ROS_WARN("Robot starting pose is outside of the map, can't find a path");
+    return false;
+  }
 
-    ROS_INFO("Finding path...");
-    ROS_INFO("layer used: %s", slope_layer.c_str());
+  // Validating that the goal pose is within the map
+  pos = grid_map::Position(goal.pose.position.x, goal.pose.position.y);
+  if (search_gridmap.isInside(pos))
+    goal_pos = node(pos, search_gridmap);
+  else {
+    ROS_WARN("Goal pose is outside of the map, can't find a path");
+    return false;
+  }
 
-    // Clear all the existing markers from the previous path
-    marker.type = visualization_msgs::Marker::DELETEALL;
+  std::deque<node> open_list, closed_list;
+
+  open_list.push_back(start_pos);
+
+  int cntr = 0;
+
+  while (open_list.size() && ros::ok()) {
+    std::sort(open_list.begin(), open_list.end(), node_sort);
+
+    node *current = new node(open_list[0]);
+
+    open_list.pop_front();
+    closed_list.push_back(*current);
+
+    marker.id = ++cntr;
+    marker.pose.position.x = current->position.x();
+    marker.pose.position.y = current->position.y();
+    marker.pose.position.z = current->height;
     marker_publisher.publish(marker);
 
-    // gridmap to use for finding the path
-    // grid_map::GridMap &search_gridmap = full_gridmap; //full_gridmap, raw_gridmap or filtered_gridmap
-    grid_map::GridMap search_gridmap = raw_gridmap;
-    
-    node start_pos, goal_pos;
-    grid_map::Position pos;
-
-    // Validating that the starting pose is within the map
-    pos = grid_map::Position(start.pose.position.x, start.pose.position.y);
-    if (search_gridmap.isInside(pos))
-        start_pos = node(pos, search_gridmap);
-    else
+    if (current->index(0) == goal_pos.index(0) &&
+        current->index(1) ==
+            goal_pos.index(1)) // Index has been choosen instead of position to
+                               // avoid comparing floats
     {
-        ROS_WARN("Robot starting pose is outside of the map, can't find a path");
-        return false;
+      ROS_INFO("Plan found");
+
+      if (get_path(current, goal, plan)) {
+        ROS_INFO("Plan published");
+        ROS_INFO("Total time: %f", (ros::Time::now() - time).toSec());
+        return true;
+      }
     }
 
-    // Validating that the goal pose is within the map
-    pos = grid_map::Position(goal.pose.position.x, goal.pose.position.y);
-    if (search_gridmap.isInside(pos))
-        goal_pos = node(pos, search_gridmap);
-    else
-    {
-        ROS_WARN("Goal pose is outside of the map, can't find a path");
-        return false;
+    for (auto c : children) {
+      grid_map::Index temp_index(current->index(0) + c(0),
+                                 current->index(1) + c(1));
+      node child =
+          node(temp_index, search_gridmap, elevation_layer, slope_layer);
+
+      int pos_in_list = is_in_list(closed_list, child);
+      if (search_gridmap.isValid(temp_index, "elevation") &&
+          pos_in_list == -1 &&
+          child.traversable) // Checking if the index is within the map
+      {
+        child.calc_f(current, goal_pos, factors);
+
+        pos_in_list = is_in_list(open_list, child);
+
+        if (pos_in_list == -1 ||
+            (pos_in_list >= 0 && open_list[pos_in_list].g > child.g))
+          open_list.push_back(child);
+      }
     }
+  }
 
-    std::deque<node> open_list, closed_list;
+  ROS_INFO("No plan found");
+  ROS_INFO("Total time: %f", (ros::Time::now() - time).toSec());
 
-    open_list.push_back(start_pos);
-
-    int cntr = 0;
-    
-    while (open_list.size() && ros::ok())
-    {
-        std::sort(open_list.begin(), open_list.end(), node_sort);
-
-        node *current = new node(open_list[0]);
-
-        open_list.pop_front();
-        closed_list.push_back(*current);
-        
-        marker.id = ++cntr;
-        marker.pose.position.x = current->position.x();
-        marker.pose.position.y = current->position.y();
-        marker.pose.position.z = current->height;
-        marker_publisher.publish(marker);
-
-        if (current->index(0) == goal_pos.index(0) && current->index(1) == goal_pos.index(1)) // Index has been choosen instead of position to avoid comparing floats
-        {
-            ROS_INFO("Plan found");
-
-            if (get_path(current, goal, plan))
-            {
-                ROS_INFO("Plan published");
-                ROS_INFO("Total time: %f", (ros::Time::now() - time).toSec());
-                return true;
-            }
-        }
-
-        for (auto c : children)
-        {
-            grid_map::Index temp_index(current->index(0) + c(0), current->index(1) + c(1));
-            node child = node(temp_index, search_gridmap, elevation_layer, slope_layer);
-
-            int pos_in_list = is_in_list(closed_list, child);
-            if (search_gridmap.isValid(temp_index, "elevation") && pos_in_list == -1 && child.traversable) // Checking if the index is within the map
-            {
-                child.calc_f(current, goal_pos, factors);
-
-                pos_in_list = is_in_list(open_list, child);
-
-                if (pos_in_list == -1 || (pos_in_list >= 0 && open_list[pos_in_list].g > child.g))
-                    open_list.push_back(child);
-            }
-        }
-    }
-
-    ROS_INFO("No plan found");
-    ROS_INFO("Total time: %f", (ros::Time::now() - time).toSec());
-
-    return false;
+  return false;
 }
 }; // namespace planner_3d
